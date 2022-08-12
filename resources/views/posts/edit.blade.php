45e044d06c119e44f70b74473c436e69ac9dd28a
yukawa-git <yooosuke.0924@gmail.com>
2022-08-12 14:41:15 +0900

    8-5　ブログ投稿の編集

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts/{{ $post->id}}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
+            </div> 
+            <input type="submit" value="update"/>
+        </form>
+        <div class="back">[<a href="/posts/{{ $post->id}}">back</a>]</div>
+    </body>
+</html>
