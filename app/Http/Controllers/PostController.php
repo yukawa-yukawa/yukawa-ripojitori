d7f5ef2323ed2afdd3cbb8f03f0ba7909d772251
yukawa-git <yooosuke.0924@gmail.com>
2022-08-12 18:56:23 +0900

    8-6　ブログ投稿の削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,6 +40,12 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+    }
 }
 ?>
 
